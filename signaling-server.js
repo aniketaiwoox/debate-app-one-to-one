0b76c7d5106f9f23ab61657a5f60277277f51b96
Aniket Magdum <aniket.magdum@aiwoox.com>
2025-07-04 01:46:02 +0530

    [update]: 1. updated all methods

--- signaling-server.js
+++ signaling-server.js
@@ -33,7 +33,50 @@ io.on("connection", (socket) => {
     console.log(`User registered: ${userId} => ${socket.id}`);
   });
 
-  /// write all required methods
+  socket.on("start-call", ({ callerId, receiverId }) => {
+    const targetSocket = userSocketMap[receiverId];
+    if (targetSocket) {
+      io.to(targetSocket).emit("call-init", { callerId, receiverId });
+      console.log(`Call init sent from ${callerId} to ${receiverId}`);
+    } else {
+      console.warn(`Receiver ${receiverId} not connected yet.`);
+    }
+  });
+
+  socket.on("offer", ({ to, offer, from }) => {
+    const targetSocketId = userSocketMap[to];
+    if (targetSocketId) {
+      io.to(targetSocketId).emit("offer", { offer, from });
+      console.log(`Offer sent from ${from} to ${to}`);
+    }
+  });
+
+  socket.on("answer", ({ to, answer, from }) => {
+    const targetSocketId = userSocketMap[to];
+    if (targetSocketId) {
+      io.to(targetSocketId).emit("answer", { answer, from });
+      console.log(`Answer sent from ${from} to ${to}`);
+    }
+  });
+
+  socket.on("ice-candidate", ({ to, candidate, from }) => {
+    const targetSocketId = userSocketMap[to];
+    if (targetSocketId) {
+      io.to(targetSocketId).emit("ice-candidate", { candidate, from });
+      console.log(`ICE candidate sent from ${from} to ${to}`);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    for (const [userId, sockId] of Object.entries(userSocketMap)) {
+      if (sockId === socket.id) {
+        delete userSocketMap[userId];
+        console.log(`User disconnected: ${userId}`);
+        break;
+      }
+    }
+  });
+  
 });
 
 const PORT = 5050;
